18fc48e295874270fad1ea067299e22b483759ec
gabriel-demes <Gabrieldemes@nyu.edu>
2021-03-16 20:14:28 -0400

    Done.

---
+++ src/components/ArticleList.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Article from "./Article"
+const ArticleList = props => {
+
+    return (
+      <main>
+          {props.posts.map(post => <Article key={post.id} title={post.title} date={post.date} preview={post.preview} />)}
+      </main>)
+}
+
+export default ArticleList
